74a1bb5f992424def7f90eb8b2fdba2c225e91e2
Itha Sriram Santhosh <isrs444@gmail.com>
2023-05-11 21:07:50 +0530

    added parallax

--- script.js
+++ script.js
@@ -2,26 +2,31 @@
 
 /* ========================|| For Profile Pic (dp) ||================================= */
 var getphoto = document.querySelector('.dp');
-var colors = ['gold', 'violet', 'green', 'red']
+var headerbg = document.querySelector('#header-image');
 let i = 0;
 var temp = setInterval(function(){
     switch(i){
         case 0:
-            getphoto.style.boxShadow = '1px 1px 10px 5px gold';
+            getphoto.style.boxShadow = '1px 1px 100px 5px gold';
+            headerbg.style.boxShadow = '1px 1px 100px 5px purple';
             i++; break;
         case 1:
-            getphoto.style.boxShadow = '1px 1px 10px 5px violet';
+            getphoto.style.boxShadow = '1px 1px 100px 5px skyblue';
+            headerbg.style.boxShadow = '1px 1px 100px 5px green';
             i++; break;
         case 2:
-            getphoto.style.boxShadow = '1px 1px 10px 5px lightcoral';
+            getphoto.style.boxShadow = '1px 1px 100px 5px red';
+            headerbg.style.boxShadow = '1px 1px 100px 5px pink';
             i++; break;
         case 3:
-            getphoto.style.boxShadow = '1px 1px 10px 5px lightgreen';
+            getphoto.style.boxShadow = '1px 1px 100px 5px green';
+            headerbg.style.boxShadow = '1px 1px 100px 5px violet';
             i++; break;
         case 4:
             i = 0;
-            getphoto.style.boxShadow = '1px 1px 10px 5px hotpink';
+            getphoto.style.boxShadow = '1px 1px 100px 5px pink';
+            headerbg.style.boxShadow = '1px 1px 100px 5px orange';
             break;
     }
-}, 1000);
+}, 800);
 
