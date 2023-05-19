60b978395d2323a0ad4a7a6a4d053568734a597e
Itha Sriram Santhosh <isrs444@gmail.com>
2023-05-19 17:37:28 +0530

    added smooth scroll css

--- script.js
+++ script.js
@@ -13,22 +13,22 @@ var temp = setInterval(function(){
             getphoto.style.boxShadow = '1px 1px 100px 5px gold';
             headerbg.style.boxShadow = '1px 1px 100px 5px purple';
             i++; break;
-        case 1:
-            getphoto.style.boxShadow = '1px 1px 100px 5px white';
-            headerbg.style.boxShadow = '1px 1px 100px 5px green';
-            i++; break;
-        case 2:
-            getphoto.style.boxShadow = '1px 1px 100px 5px aqua';
-            headerbg.style.boxShadow = '1px 1px 100px 5px gold';
-            i++; break;
-        case 3:
-            getphoto.style.boxShadow = '1px 1px 100px 5px green';
-            headerbg.style.boxShadow = '1px 1px 100px 5px violet';
-            i++; break;
-        case 4:
-            i = 0;
-            getphoto.style.boxShadow = '1px 1px 100px 5px pink';
-            headerbg.style.boxShadow = '1px 1px 100px 5px orange';
+            case 1:
+                getphoto.style.boxShadow = '1px 1px 100px 5px white';
+                headerbg.style.boxShadow = '1px 1px 100px 5px green';
+                i++; break;
+            case 2:
+                getphoto.style.boxShadow = '1px 1px 100px 5px aqua';
+                headerbg.style.boxShadow = '1px 1px 100px 5px gold';
+                i++; break;
+            case 3:
+                getphoto.style.boxShadow = '1px 1px 100px 5px green';
+                headerbg.style.boxShadow = '1px 1px 100px 5px violet';
+                i++; break;
+            case 4:
+                i = 0;
+                getphoto.style.boxShadow = '1px 1px 100px 5px pink';
+                headerbg.style.boxShadow = '1px 1px 100px 5px orange';
             break;
     }
 }, 800);
