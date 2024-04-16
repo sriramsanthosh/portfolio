be79e765762d58d53bca378d14241c0c616739c2
Itha Sriram Santhosh <isrs444@gmail.com>
2024-04-16 22:20:58 +0530

    removed octokit

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-const { Octokit } = require("@octokit/core");
 // import { BrowserRouter } from 'react-router-dom'
 
 
